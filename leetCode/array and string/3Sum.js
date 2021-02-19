8050e44596d7c22ae94d867189f27cacec309fcd
Jared <jaredarodriguez@gmail.com>
2021-02-19 08:37:00 -0500

    creates leetCode array and string folder

---
+++ leetCode/array and string/3Sum.js
@@ -0,0 +1,41 @@
+/*
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Notice that the solution set must not contain duplicate triplets.
+
+*/
+
+function threeSum(arr) {
+  arr.sort((a, b) => a - b)
+  let tripSum = []
+  for (let curr = 0; curr < arr.length - 2; curr++) {
+    if (arr[curr] != arr[curr - 1]) {
+      let next = curr + 1
+      let end = arr.length - 1
+      while (next < end) {
+        const result = arr[curr] + arr[next] + arr[end]
+        if (result === 0) {
+          tripSum.push([arr[curr], arr[next], arr[end]])
+          while (arr[next] == arr[next + 1]) {
+            next++
+          }
+          while (arr[end] == arr[end - 1]) {
+            end--
+          }
+          next++
+          end--
+        } else if (result < 0) {
+          next++
+        } else if (result > 0) {
+          end--
+        }
+      }
+    }
+  }
+  return tripSum
+}
+
+let exampleArr = [-1, 0, 1, 2, -1, 4]
+
+console.log(threeSum(exampleArr))
