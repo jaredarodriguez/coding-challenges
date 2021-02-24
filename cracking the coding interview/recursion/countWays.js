4691b89172bbdb0d1fee68a1e3b3ae4f4481a6ad
Jared <jaredarodriguez@gmail.com>
2021-02-24 08:04:35 -0500

    solves countWays recursion

---
+++ cracking the coding interview/recursion/countWays.js
@@ -0,0 +1,17 @@
+/*
+
+A child is running up a staircase with n steps, and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
+
+*/
+
+function countWays(n) {
+  if (n < 0) {
+    return 0
+  } else if (n == 0) {
+    return 1
+  } else return countWays(n - 1) + countWays(n - 2) + countWays(n - 3)
+}
+
+console.log(countWays(2))
+console.log(countWays(1))
+console.log(countWays(5))
