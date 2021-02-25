bbf2167efbf16d9b24e35c0b151646318873ca48
Jared <jaredarodriguez@gmail.com>
2021-02-25 09:19:05 -0500

    solves using while loop

--- leetCode/array and string/removeDupFromSortedArray.js
+++ leetCode/array and string/removeDupFromSortedArray.js
@@ -15,3 +15,21 @@ var removeDuplicates = function (nums) {
   }
   return index
 }
+
+//  using while loop
+
+var removeDuplicates = (nums) => {
+  if (nums.length == 0) return 0
+  let i = 0
+  let j = 1
+  while (j < nums.length) {
+    if (nums[i] != nums[j]) {
+      i++
+      nums[i] = nums[j]
+      j++
+    } else {
+      j++
+    }
+  }
+  return i + 1
+}
