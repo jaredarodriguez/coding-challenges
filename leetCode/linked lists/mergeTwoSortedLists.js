10558064c02d3c7daed13439e6033acce5df471e
Jared <jaredarodriguez@gmail.com>
2021-03-04 10:22:45 -0500

    edit

--- leetCode/linked lists/mergeTwoSortedLists.js
+++ leetCode/linked lists/mergeTwoSortedLists.js
@@ -9,7 +9,7 @@ function ListNode(val, next) {
 }
 
 var mergeTwoLists = function (l1, l2) {
-  let dummy = ListNode(-1)
+  let dummy = new ListNode(-1)
   let head = dummy
   while (l1 !== null && l2 !== null) {
     if (l1.val < l2.val) {
