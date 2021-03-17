05df6c2031f0774838b17238fa9e98440ff68331
Jared <jaredarodriguez@gmail.com>
2021-03-17 10:59:26 -0400

    Kadane Algo

--- daily coding problem/striver-180-day-challenge/day1.js
+++ daily coding problem/striver-180-day-challenge/day1.js
@@ -214,4 +214,19 @@ function maxSum(arr) {
   return maxSum
 }
 
-console.log(maxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+function maxKadaneSum(arr) {
+  let sum = 0
+  let maxSum = arr[0]
+
+  for (let i = 1; i < arr.length; i++) {
+    sum += arr[i]
+    if (sum > maxSum) {
+      maxSum = sum
+    } else if (sum < 0) {
+      sum = 0
+    }
+  }
+  return maxSum
+}
+
+console.log(maxKadaneSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
