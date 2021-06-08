fcfdfa19f58d3bfb2e81d7ac6ac78f43934fea5c
Jared Rodriguez <43504354+jrodriguez082046@users.noreply.github.com>
2021-06-08 09:03:20 -0400

    records traversal order notes

---
+++ leetCode/treesAndGraphs/notes.js
@@ -0,0 +1,10 @@
+/*
+
+Pre-order Traversal: root ==> left ==> right
+
+In-order Traversal : left ==> root ==> right (can retrieve all data in sorted order)
+
+Post-order Traversal : left ==> right ==> root (delete order - i.e. delete a nodes lefts childe and right child before deleting self)
+    - also used in mathematical expressions as it's easier to write this way
+
+*/
